c4af180e57b03c674f5ad8d0e0359dbfcc1d10dd
iamcody-git <iamcody.github@gmail.com>
2025-01-23 22:14:42 +0545

    logo part added

---
+++ src/components/ui/Logo.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Logo = () => {
+  return (
+    <>
+      <h1 className='font-extrabold text-2xl text-green-300'>CodyForm</h1>
+    </>
+  )
+}
+
+export default Logo
